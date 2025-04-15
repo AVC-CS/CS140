f380a2d2198f92b9759a2ab9d933f90cbf2cd170
Kyu Lee <kwooler@gmail.com>
2025-04-14 23:48:52 -0700

    CS140 M1Q2 Update

--- ASM/M1Q2.asm
+++ ASM/M1Q2.asm
@@ -8,8 +8,11 @@ main:
     li      $v0, 2 	# print floating service code
     lwc1 	$f5, val1
     lwc1 	$f7, val2
+    mfc1    $t0, $f5
+    mfc1    $t1, $f7
     sub.s 	$f12, $f5, $f7
     mfc1    $t0, $f12
+    add     $s0, $t0, $t1
     li 	$v0, 2		# print service for floating
     syscall
 ## End of file
